8b8b9427fcdd2a94cbc38b0d93d3bb613106c734
joemar25 <joemar.cardino000@gmail.com>
2024-06-30 15:39:49 +0800

    chore: Update page titles for create account and forgot password pages

---
+++ assets/js/controls/student/class.js
@@ -0,0 +1,70 @@
+const SUPABASE_URL = "https://hvqvmxakmursjidtfmdj.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2cXZteGFrbXVyc2ppZHRmbWRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg2MDA4MjQsImV4cCI6MjAzNDE3NjgyNH0.dykJM61G-58LEnAyCUU6-irano2f4vraV8t1l8C5KZ8";
+
+let supabase_connection;
+
+document.addEventListener("DOMContentLoaded", async () => {
+  initializeSupabase();
+  await fetchClasses();
+});
+
+function initializeSupabase() {
+  supabase_connection = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+}
+
+async function fetchClasses() {
+  const studentId = 3;
+
+  try {
+    const { data: classes, error } = await supabase_connection
+      .from("class_students")
+      .select(
+        `
+        *,
+        classes (
+          *,
+          courses ( id, title )
+        )
+      `
+      )
+      .eq("student_id", studentId);
+
+    if (error) throw error;
+
+    displayClasses(classes);
+  } catch (error) {
+    console.error("Error fetching classes:", error);
+    alert("Failed to fetch classes. Please try again.");
+  }
+}
+
+function displayClasses(classStudents) {
+  const classCardsContainer = document.getElementById("classCards");
+  classCardsContainer.innerHTML = "";
+
+  classStudents.forEach((classStudent) => {
+    const class_ = classStudent.classes;
+    const card = document.createElement("div");
+    card.className =
+      "flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800";
+    card.innerHTML = `
+      <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
+        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+          <path
+            d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z">
+          </path>
+        </svg>
+      </div>
+      <div>
+        <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+          ${class_.courses.title}
+        </p>
+        <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+          ${class_.class_name}
+        </p>
+      </div>
+    `;
+    classCardsContainer.appendChild(card);
+  });
+}
